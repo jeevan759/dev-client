136482e4e6d2ce7d4142b67303f630fe7b319a4d
Gouthamreddy2000 <mandlagouthamreddy@gmail.com>
2021-06-14 23:16:37 +0530

    added google signin

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var session = require('express-session');
 var dbconnect = require('./backend/lib/connectLib');
 var config = require('./backend/config/config');
+var user=require('./backend/models/registrationModel');
 require('./backend/lib/dbUsersBootstrap').createUsers();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,22 +23,52 @@ app.get('/login',function(req,res){
     htmlBase=__dirname+'/public/index.html';
     res.sendFile(htmlBase);
 })
-/*app.post('/api/login',function(req,res){
-    userLib.isUserValid(req.body,function(resultJson){
-        if(resultJson.success==true){
-          //console.log("Session for User Initialized");
-          //req.session.user = {username: resultJson.username};
-        }
-        res.json(resultJson);
-       })
-})*/
 app.use(session({
-    resave:false, 
-    saveUninitialized:false, 
-    secret:config.sessionSecret, 
-    store: MongoStore.create({ mongoUrl: config.mongoConnectionString })
-}));
+    secret: 'goutham',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000
+    },
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_CONNECT_STRING })
+  
+  }))
+  
+  app.post('/api/login', function(req, res) {
+    user.find(req.body, function(err, data) {
+        var response = {success: false, message: 'Login Failed', user: null };
+        if (err) { res.status(400).json({ msg: "Failed" }); } else if (data.length == 1) {
+            req.session.userid = data[0]._id
+            req.session.username = data[0].username
+            console.log(req.session)
+            response.success = true;
+        response.message = 'Login Successful';
+        response.user = {username: req.session.username};
+            res.json(response);
+            
 
+        } else {
+
+            res.redirect("/login");
+
+        }
+    });
+})
+var isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.userid)
+        next();
+    else
+        return res.redirect("/login");
+}
+var isNotAuthenticated = (req, res, next) => {
+    if (!req.session || !req.session.userid)
+        next();
+    else
+        return res.redirect("/");
+}
+app.get("/", isAuthenticated, (req, res) => {
+    res.sendFile(__dirname + "/public/sample.html")
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.get('/check',function(req,res){
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,20 @@ var express = require('express');
 var router = express.Router();
 var userLib=require('../backend/lib/userLib');
 var user=require('../backend/models/registrationModel');
+const CLIENT_ID= '445093466494-u1c7jg8178j553gv6o6ao8uohia3cja6.apps.googleusercontent.com';
+const {OAuth2Client} = require('google-auth-library');
+var session = require('express-session');
+const client = new OAuth2Client(CLIENT_ID);
+const cookieParser=require('cookie-parser');
+router.use(cookieParser());
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/sample',checkAuthenticated,function(req,res){
+  let user=req.user;
+  res.render('sample',{user});
+})
 router.post('/api/login',function(req,res){
   userLib.isUserValid(req.body,function(resultJson){
       if(resultJson.success==true){
@@ -14,6 +24,24 @@ router.post('/api/login',function(req,res){
       }
       res.json(resultJson);
      })
+})
+router.post('/login',function(req,res){
+  let token=req.body.token;
+  console.log(token);
+  async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  console.log(payload);
+}
+verify()
+.then(()=>{
+    res.cookie('session-token',token);
+    res.send('success');
+}).catch(console.error);
 })
 router.post("/regis",function(req,res){
   var username=req.body.username;
@@ -52,5 +80,27 @@ router.get('/mailbox', function(req, res) {
       res.json({success:false, message:'Need to be logged in to get mailbox'});
   }
 });
+function checkAuthenticated(req,res,next){
+  let token=req.cookies['session-token'];
+  let user={};
+  async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  
+  });
+  const payload = ticket.getPayload();
+  user.name=payload.name;
+  user.email=payload.email;
+  user.picture=payload.picture;
+  //console.log(payload);
+}
+verify()
+.then(()=>{
+    req.user=user;
+    next();
+}).catch(err=>{
+    res.redirect('/login');
+})
+}
 
 module.exports = router;
